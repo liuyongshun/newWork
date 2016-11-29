865d2547b363616d4b1475e9f8e3560904f70c2c
liuyongshun <liuys0902@gmail.com>
2016-11-29 10:50:18 +0800

    node

---
+++ node/one/index.js
@@ -0,0 +1,17 @@
+
+var
+ http = require('http');
+http.createServer(
+function
+(req,res){
+        res.writeHead(200, { 'Content-Type': 'text/plain' }); 
+        res.end('Hello world!');
+}).listen(3000);
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+
+
+
+var http = require( 'http' );
+http.createSever( function( res, req ) {
+    var path = req.url.replace(  )
+} )
