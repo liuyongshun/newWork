136f744da742c14bd17b64e932105378202f89a9
liuyongshun <liuys0902@gmail.com>
2016-12-10 17:10:39 +0800

    regExp

--- regExp/one/js/index.js
+++ regExp/one/js/index.js
@@ -48,7 +48,7 @@
 
 
 
-
+// judge email format.
 // var checkEmail = {
 //     // judges为提交按钮，
 //     getSelect: document.getElementById( 'judges' ),
@@ -81,8 +81,9 @@
 // }
 // checkEmail.method();
 
-// ====================practice rank========================================================
-// var str = "hat hot my telephone is 18330234567 email: liulfjs@126.com\n" 
+
+// // ====================practice rank========================================================
+var str = "hat hot my telephone is 12202345672  10 email: liulfjs@126.com \n liuyongshun this his at" 
 // // search 括号内任意字符，为设置g时，找到ema中的任何一个都会停止。[a-z]
 // var patt = /[ema]/;
 // console.log( str.match( patt ) );
@@ -109,10 +110,49 @@
 // console.log( str.match( patt7 ) );
 // var patt8 = /\D/g;
 // console.log( str.match( patt8 ) );
-// // 查找空白字符
-
-
-// test meet
-var str = "hels///// d?"
-var patt = /\/?(?:\?.*)?$/g;
-console.log( patt.exec( str ) )
+// // 查找空白字符,包括回车
+// var patt9 = /\s/g;
+// console.log( str.match( patt9 ) );
+// var patt00 = /\S/g;
+// console.log( str.match( patt00 ) );
+// // 查找换行符\n
+// var patt01 = /\n/g;
+// console.log( str.match( patt01 ) );
+// console.log( str.search( patt01 ) );  //++++++++++++++++++
+// // 匹配任何包含至少一个h的字符串 h+
+// var patt02 = /h+/g;
+// console.log( str.match( patt02 ) );
+// var patt03 = /\w+/g;  //\w指匹配单词字符。
+// console.log( str.match( patt03 ) )
+// // 匹配任何结尾（整个字符串结尾）为t的字符。t$
+// var patt04 = /t$/g;
+// console.log( str.match( patt04 ) );
+// // 匹配任何开始（整个字符串结尾）为h的字符。^h
+// var patt05 = /^h/g;
+// console.log( str.match( patt05 ) );
+// // 匹配包含 X 个 n 的序列的字符串 n{X}
+// // n{X,Y}匹配包含 X 或 Y 个 n 的序列的字符串。(筛选千和万和十万和百万)
+// // n{X,}  匹配包含至少 X 个 n 的序列的字符串。
+// var patt06 = /\d{11}/g;
+// console.log( str.match( patt06 ) );
+// // n? 匹配任何包含零个或一个 n 的字符串。
+// var patt07 = /10?/g; //这里指的零个或一个0。
+// console.log( str.match( patt07 ) );
+// // ?=n 匹配任何其后紧接指定字符串 n 的字符串。
+// var patt08 = /one(?= is)/g;
+// console.log( str.match( patt08 ) );
+// // ?!n 匹配其后没有紧接指定字符串 n 的任何字符串。
+// var patt09 = /is(?! at)/g;
+// console.log( str.match( patt09 ) );
+// console.log( patt09.exec( str ) );  //++++++++++++++++++++
+// console.log( patt09.test( str ) );  //++++++++++++++++++++
+// var str2 = "Mr Blue has a blue house and a blue car";
+// var n = str2.replace(/blue/gi,"red");  //+++++++++++++++++
+// console.log( n );
+// var patt10 = /./g;
+// // exec匹配非全局时与match相同，匹配全局时需要反复多次调用。
+// console.log( patt10.exec( str ) );
+// console.log( patt10.exec( str ) );
+// console.log( str.match( patt10 ) );
+var patt11 = /h(?:at|ot)/g;
+console.log( str.match( patt11 ) );
